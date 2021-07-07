97e3d8a8a3e2fedf261b2a8c76d1b5b33b3efbeb
Amir Tadrisi <amirtds@gmail.com>
2021-07-07 10:53:50 -0500

    chore: add purgecss configs to the tailwind configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,42 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  future: {
+    purgeLayersByDefault: true,
+    applyComplexClasses: true,
+  },
+  purge: {
+    content: [
+      './src/components/**/*.{js,ts,jsx,tsx}',
+      './src/pages/**/*.{js,ts,jsx,tsx}',
+    ],
+    options: {
+      safelist: {
+        standard: [/^text-/, /^bg-/, /^hover:border-/, /^focus:/, /^border-/, /^hover:text-/, /^hover:bg-/],
+      },
+    },
+  },
   theme: {
+    screens: {
+      'xs': '100px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    },
+    colors: {
+      rose: colors.rose,
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.coolGray,
+      blue: colors.blue
+    },
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
