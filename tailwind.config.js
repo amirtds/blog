9b52e8e9a71161c0b63da1799ad7584fdb7f2b3b
Amir Tadrisi <amirtds@gmail.com>
2021-07-07 16:26:45 -0500

    chore: remove purge css for development environment

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,38 +3,10 @@ const colors = require('tailwindcss/colors')
 
 module.exports = {
   future: {
-    purgeLayersByDefault: true,
-    applyComplexClasses: true,
   },
   purge: {
-    content: [
-      './src/components/**/*.{js,ts,jsx,tsx}',
-      './src/pages/**/*.{js,ts,jsx,tsx}',
-    ],
-    options: {
-      safelist: {
-        standard: [/^text-/, /^bg-/, /^hover:border-/, /^focus:/, /^border-/, /^hover:text-/, /^hover:bg-/],
-      },
-    },
   },
   theme: {
-    screens: {
-      'xs': '100px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-    },
-    colors: {
-      rose: colors.rose,
-      transparent: 'transparent',
-      current: 'currentColor',
-      black: colors.black,
-      white: colors.white,
-      gray: colors.coolGray,
-      blue: colors.blue
-    },
-    extend: {},
   },
   variants: {},
   plugins: [
